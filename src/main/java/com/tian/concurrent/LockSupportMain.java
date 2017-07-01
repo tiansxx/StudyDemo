5ea43ae4f713005c97f10caac979c0eb6cc5da8f
tiansxx <fengyue_ok@163.com>
2017-07-01 12:57:11 +0800

    LockSupport park unpark

--- src/main/java/com/tian/concurrent/LockSupportMain.java
+++ src/main/java/com/tian/concurrent/LockSupportMain.java
@@ -5,13 +5,45 @@ import java.util.concurrent.locks.LockSupport;
 public class LockSupportMain {
 
 	public static void main(String[] args) {
-		
+		System.out.println("LockSupport start.");
+		parkOther();
+		parkSelf();
+		System.out.println("LockSupport end.");
+	}
+
+	private static void parkSelf() {
 		System.out.println("start");
 //		如果许可未被占用,则方法立即返回,否则阻塞当前线程;许可默认被占用
 //		LockSupport不可重入,如果同一线程2次调用该park方法将被阻塞
 		LockSupport.park();
 		System.out.println("end");
+	}
+	
+	private static void parkOther() {
+		Thread t = new Thread(new Task(), "park other");
+		t.start();
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+//		给t线程释放许可
+		LockSupport.unpark(t);
+	}
+	
+	private static class Task implements Runnable {
+
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + " start.");
+			
+			LockSupport.park();
+			
+			System.out.println(Thread.currentThread().getName() + " end.");
+		}
 		
 	}
+	
+	
 	
 }
